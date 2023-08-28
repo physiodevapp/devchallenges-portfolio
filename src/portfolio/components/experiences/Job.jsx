9d7eaafd0f43cd33857e32f77c5f245743fc4fd2
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2023-08-28 20:47:00 +0200

    style - Margin in Job component

--- src/portfolio/components/experiences/Job.jsx
+++ src/portfolio/components/experiences/Job.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export const Job = () => {
   return (
     <>
-      <div className="card mb-3 border-0">
+      <div className="card mt-2 border-0 ">
         <div className="row g-0">
-          <div className="col-md-4">
-            <img src="..." className="img-fluid rounded-start" alt="..." />
+          <div className="col-md-4 p-2 pe-0">
+            <img src="https://images.hola.com/imagenes/mascotas/20220729214547/pajaro-dodo-curiosidades-ave-extinguida-dn/1-121-94/curiosidades-sobre-la-extincion-del-pajaro-dodo-t.jpg?tx=w_680" className="img-fluid rounded-start" alt="..." />
           </div>
           <div className="col-md-8">
-            <div className="card-body">
+            <div className="card-body pt-0">
               <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
               <h5 className="card-title">Job title</h5>
               <p className="card-text">Fugiat labore sunt reprehenderit cupidatat in sit et ea voluptate do pariatur est nostrud dolor.</p>
